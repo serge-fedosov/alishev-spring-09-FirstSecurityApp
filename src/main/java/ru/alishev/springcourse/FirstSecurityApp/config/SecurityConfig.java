ff790ad27e36004869aa27e5a1e81f00f2021c53
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-03 23:38:25 +0400

    12/86 add role

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -26,8 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Конфигурируем Spring Security
         // Конфигурируем авторизацию
         http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
