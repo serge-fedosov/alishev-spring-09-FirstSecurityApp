e84219e29b2fe2304f18a6672473994794dcd3a6
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-03 17:57:49 +0400

    12/79 Spring Security - get user info

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/HelloController.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package ru.alishev.springcourse.FirstSecurityApp.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.alishev.springcourse.FirstSecurityApp.security.PersonDetails;
 
 @Controller
 public class HelloController {
@@ -10,4 +14,14 @@ public class HelloController {
     public String sayHello() {
         return "hello";
     }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+    }
+
 }
